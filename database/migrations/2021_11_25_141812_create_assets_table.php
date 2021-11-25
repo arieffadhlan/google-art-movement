81da4e64c13a4fe40b850940453145a96ff39395
yefthaaa <yeftha.imani@gmail.com>
2021-11-25 22:17:07 +0700

    Pesan Commit anda (misal: add stock)

---
+++ database/migrations/2021_11_25_141812_create_assets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('desc')->nullable();
+            $table->text('detail');
+            $table->string('image');
+            $table->string('entites_id');
+            $table->string('artist_id');
+            $table->YEAR('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
---
+++ database/migrations/2021_11_25_142700_create_exibits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExibitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exibits', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('detail');
+            $table->string('image');
+            $table->string('entites_id');
+            $table->string('partner_id');
+            $table->string('date'); //karena pelaksanaan pameran berupa tgl (2 des 2020 - 5 des 2020)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exibits');
+    }
+}
